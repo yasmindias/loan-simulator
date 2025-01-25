34ac99a8ff6577a9c09bd9190fd06ccfb121bc97
Yasmin Dias <yasmindias.info@gmail.com>
2025-01-25 20:14:21 -0300

    Initial project configuration

---
+++ src/main/kotlin/com/project/loan_simulator/LoanSimulatorApplication.kt
@@ -0,0 +1,11 @@
+package com.project.loan_simulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoanSimulatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoanSimulatorApplication>(*args)
+}
