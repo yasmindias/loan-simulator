8ecbda128978a83986aac179656cd4124e7c1708
Yasmin Dias <yasmindias.info@gmail.com>
2025-01-27 20:16:00 -0300

    adds performance test module

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "1.9.25"
+	id("io.gatling.gradle") version "3.13.3"
 }
 
 group = "com.project"
